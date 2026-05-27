266148fc864c19e826a86565326cce37e0ad99f6
ronakg2001 <ronakg.2001@gmail.com>
2026-05-27 13:57:55 +0530

    chore: remove wrangler.jsonc configuration file

--- web_frontend/mobile_navigation_tailwind.js
+++
@@ -1,9 +0,0 @@
-tailwind.config = {
-            theme: {
-                extend: {
-                    colors: {
-                        slate: { 850: '#151e2e', 900: '#0f172a' }
-                    }
-                }
-            }
-        };
