9902e3ed6d12e1cfb8cf7e2d104308b556dad7a4
ronakg2001 <ronakg.2001@gmail.com>
2026-06-10 13:14:30 +0530

    feat: configure next.config.ts for static site export and environment variables

--- web/next.config.ts
+++ web/next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  env: {
+    R2_STATIC_MEDIA_BUCKET_PUBID: process.env.R2_STATIC_MEDIA_BUCKET_PUBID,
+    R2_USERMEDIA_BUCKET_PUBID: process.env.R2_USERMEDIA_BUCKET_PUBID,
+  },
 };
 
 export default nextConfig;
